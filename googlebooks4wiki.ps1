66ce01b5c84fd1aa8b25e382c82d4b70901ca946
E. Fishgalov <eugrus@gmail.com>
2023-01-18 04:02:40 +0100

    Create googlebooks4wiki.ps1
    port from bash to PowerShell

---
+++ googlebooks4wiki.ps1
@@ -0,0 +1,42 @@
+[Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8 # Unicode output doesn't seem to work though
+
+# Pass ISBN and optinally the subdomain of a supported language edition of Wikipedia as command line arguments
+$isbn = $args[0]
+$lang = $args[1]
+
+# Use Google Books API to retrieve book information
+$data = Invoke-WebRequest -Uri "https://www.googleapis.com/books/v1/volumes?q=isbn:$isbn" -Method Get
+
+# Data extraction from API response
+$json = $data.Content | ConvertFrom-Json
+$title = $json.items[0].volumeInfo.title
+$authors = $json.items[0].volumeInfo.authors -join ", "
+$publisher = $json.items[0].volumeInfo.publisher
+$date = $json.items[0].volumeInfo.publishedDate
+$year = $date.Substring(0,4)
+
+# Template generation
+
+# for ru.wikipedia.org as an option
+if ($lang -eq "ru") {
+  $template = "{{Книга
+  |название=$title
+  |автор=$authors
+  |год=$year
+  |издательство=$publisher
+  |isbn=$isbn
+  |ref=
+  }}"
+# for en.wikipedia.org as a default
+} else {
+  $template = "{{Cite book
+  |title=$title
+  |author=$authors
+  |date=$date
+  |publisher=$publisher
+  |isbn=$isbn
+  |ref=
+  }}"
+}
+
+Write-Output $template
