ccdfaf12d52b02be7f3f8d361dbac842d70d07fe
Evgeny Fishgalov <eugrus@gmail.com>
2023-06-16 14:31:51 +0200

    Update googlebooks4wiki.sh
    support for CGI webservers

--- googlebooks4wiki.sh
+++ googlebooks4wiki.sh
@@ -1,41 +1,56 @@
-#!/bin/bash
+#!/bin/bash 
+  
+ # Pass ISBN and optionally the subdomain of a supported language edition of Wikipedia as command line arguments or get them from URI when invoked by a webserver
 
-# Pass ISBN and optionally the subdomain of a supported language edition of Wikipedia as command line arguments
-isbn=$1
-lang=$2
-
-# Use Google Books API to retrieve book information
-data=$(curl -s "https://www.googleapis.com/books/v1/volumes?q=isbn:$isbn")
-
-# Data extraction from API response
-title=$(echo $data | jq -r '.items[0].volumeInfo.title')
-author=$(echo $data | jq -r '.items[0].volumeInfo.authors | join(", ")')
-publisher=$(echo $data | jq -r '.items[0].volumeInfo.publisher')
-date=$(echo $data | jq -r '.items[0].volumeInfo.publishedDate')
-year=${date:0:4}
-
-# Template generation
-
-# for ru.wikipedia.org as an option
-if [[ $lang == "ru" ]]; then
-  template="{{Книга
-  |название=$title
-  |автор=$author
-  |год=$year
-  |издательство=$publisher
-  |isbn=$isbn
-  |ref=
-  }}"
-# for en.wikipedia.org as a default
+if [ -t 0 ]; then
+  # Running in a terminal
+  isbn="$1"
+  lang="$2"
 else
-  template="{{Cite book
-  |title=$title
-  |author=$author
-  |date=$date
-  |publisher=$publisher
-  |isbn=$isbn
-  |ref=
-  }}"
-fi
+  # Running as a script invoked by a webserver
+  declare -A params
+  IFS='&' read -ra query_params <<< "$QUERY_STRING"
+  for param in "${query_params[@]}"; do
+    IFS='=' read -r key value <<< "$param"
+    params["$key"]="$value"
+  done
 
-echo "$template"
+  isbn="${params["isbn"]}"
+  lang="${params["lang"]}"
+fi
+  
+ # Use Google Books API to retrieve book information 
+ data=$(curl -s "https://www.googleapis.com/books/v1/volumes?q=isbn:$isbn") 
+  
+ # Data extraction from API response 
+ title=$(echo $data | jq -r '.items[0].volumeInfo.title') 
+ author=$(echo $data | jq -r '.items[0].volumeInfo.authors | join(", ")') 
+ publisher=$(echo $data | jq -r '.items[0].volumeInfo.publisher') 
+ date=$(echo $data | jq -r '.items[0].volumeInfo.publishedDate') 
+ year=${date:0:4} 
+  
+ # Template generation 
+  
+ # for ru.wikipedia.org as an option 
+ if [[ $lang == "ru" ]]; then 
+   template="{{Книга 
+   |название=$title 
+   |автор=$author 
+   |год=$year 
+   |издательство=$publisher 
+   |isbn=$isbn 
+   |ref= 
+   }}" 
+ # for en.wikipedia.org as a default 
+ else 
+   template="{{Cite book 
+   |title=$title 
+   |author=$author 
+   |date=$date 
+   |publisher=$publisher 
+   |isbn=$isbn 
+   |ref= 
+   }}" 
+ fi 
+  
+ echo "$template"
